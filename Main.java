1d170e72fa52d3c9610c1ce0c976cbb582174562
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-05-26 07:29:09 -0400

    testing

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import inventory.Vehicle;
+import inventory.Truck;
 
 class Main {
   public static void main(String[] args) {
-    Vehicle myVehicle = new Vehicle("Honda", "RCV");
+    // Vehicle myVehicle = new Vehicle("Honda", "RCV");
     // myVehicle.make = "Honda";
     // myVehicle.model = "RCV";
 
-    System.out.println(myVehicle);
+    Truck myTruck = new Truck("Ford", "F150", (short) 500, true);
+
+    System.out.println(myTruck);
   }
 }
