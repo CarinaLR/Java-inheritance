4536c1230ebe55086219b738e956e95e66df2f50
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-05-27 12:24:22 -0400

    add constructor & properties

--- inventory/Promotion.java
+++ inventory/Promotion.java
@@ -2,4 +2,15 @@ package inventory;
 
 public class Promotion extends Vehicle {
 
+  // Properties
+  private String offer;
+  private int discount;
+
+  // Constructor
+  public Proomotion(String make, String model, String offer, int discount) {
+    super(make, model);
+    this.offer = offer;
+    this.discount = discount;
+  }
+
 }
